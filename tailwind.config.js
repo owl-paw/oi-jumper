4244ab1f1f6b81b2a0ffb1de2374fc241e5014d6
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-05-04 14:11:03 +0800

    dark mode enhancements
    and fine-tuning Hitokoto display
    and fallback fonts for better Simplified Chinese text display

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mono: ['"JetBrains Mono"', "Consolas", "Menlo", "monospace"],
+        mono: [
+          '"JetBrains Mono"',
+          "Consolas",
+          "Menlo",
+          "monospace",
+          '"Noto Sans CJK SC"',
+          '"Source Han Sans SC"',
+          "sans-serif",
+        ],
       },
     },
   },
   plugins: [],
-  darkMode: "class",
+  darkMode: "selector",
 };
