0ef4ec32e05eb14638a3b5982e57a7432a33039b
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-06-02 02:09:12 +0800

    tweak visual aesthetics

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ function openLink(url) {
   var popUp = window.open(url);
   if (!popUp || popUp.closed || typeof popUp.closed == "undefined") {
     alert(
-      "Your browser blocked us from opening the targeted page. Try adding this site to your exception list in your browser's settings to ensure its functionality."
+      "Your browser blocked us from opening the targeted page. Try adding this site to your exception list in your browser's settings to ensure its functionality.",
     );
   }
 }
@@ -52,7 +52,7 @@ submitButtonProbId.addEventListener("click", function () {
           "https://codeforces.com/problemset/problem/" +
             match[1] +
             "/" +
-            match[2]
+            match[2],
         );
       else
         openLink(
@@ -60,7 +60,7 @@ submitButtonProbId.addEventListener("click", function () {
             match[1] +
             "/" +
             match[2] +
-            match[3]
+            match[3],
         );
       return;
     }
@@ -113,7 +113,7 @@ inputBoxProbId.addEventListener("keydown", function (event) {
 
 submitButtonProbName.addEventListener("click", function () {
   const judgeSelectorProbName = document.getElementById(
-    "judge-selector-prob-name"
+    "judge-selector-prob-name",
   );
   const inputValue = inputBoxProbName.value;
 
