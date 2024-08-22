044860b82c2144b3b75192b3740e2646740daf28
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-08-23 01:56:37 +0800

    THE TYPESCRIPT ERA

---
+++ assets/js/onload.ts
@@ -0,0 +1,185 @@
+const cacheName = "i18n-cache-v1";
+
+const languageFiles: { [index: string]: string } = {
+  en: "./assets/lang/en.json",
+  zh: "./assets/lang/zh.json",
+};
+
+function cacheLanguageFiles() {
+  if ("caches" in window) {
+    caches.open(cacheName).then((cache) => {
+      Object.values(languageFiles).forEach((file: string) => {
+        cache.add(file).catch((err) => {
+          console.error(`Failed to cache ${file}: `, err);
+        });
+      });
+    });
+  }
+}
+
+function loadLanguage(lang: string) {
+  document.documentElement.setAttribute("lang", lang);
+  const url = languageFiles[lang];
+
+  if ("caches" in window) {
+    caches
+      .match(url)
+      .then((response) => {
+        if (response) {
+          return response.json();
+        } else {
+          return fetch(url).then((response) => {
+            caches.open(cacheName).then((cache) => cache.put(url, response.clone()));
+            return response.json();
+          });
+        }
+      })
+      .then((data) => {
+        updatePageWithLanguageData(data);
+        localStorage.setItem("preferredLanguage", lang);
+        updateLanguageSwitcher(lang);
+      })
+      .catch((error) => {
+        console.error("Error loading language data: ", error);
+      });
+  } else {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        updatePageWithLanguageData(data);
+        localStorage.setItem("preferredLanguage", lang);
+        updateLanguageSwitcher(lang);
+      });
+  }
+}
+
+function updateLanguageSwitcher(lang: string) {
+  const selectedButton = document.getElementById(lang === "zh" ? "chn-btn" : "eng-btn") as HTMLButtonElement;
+  selectedButton.setAttribute("checked", "checked");
+}
+
+function updatePageWithLanguageData(data: any) {
+  (document.getElementById("input-pid") as HTMLInputElement).placeholder = data.prob_id.input;
+  (document.getElementById("submit-pid") as HTMLButtonElement).innerHTML = data.prob_id.btn;
+  (document.getElementById("input-pna") as HTMLInputElement).placeholder = data.prob_name.input;
+  (document.getElementById("submit-pna") as HTMLButtonElement).innerHTML = data.prob_name.btn;
+  (document.getElementById("github") as HTMLLinkElement).innerHTML = data.settings.github;
+  (document.getElementById("sponsor") as HTMLLinkElement).innerHTML = data.settings.sponsor;
+  (document.getElementById("system-scheme") as HTMLInputElement).ariaLabel = data.settings.auto;
+  (document.getElementById("light-scheme") as HTMLInputElement).ariaLabel = data.settings.light;
+  (document.getElementById("dark-scheme") as HTMLInputElement).ariaLabel = data.settings.dark;
+}
+
+function initLanguage() {
+  const savedLang = localStorage.getItem("preferredLanguage") || "zh";
+  loadLanguage(savedLang);
+}
+
+window.onload = function () {
+  initLanguage();
+  cacheLanguageFiles();
+};
+
+const words = [
+  "???",
+  "css",
+  "git",
+  "svg",
+  "time",
+  "code",
+  "love",
+  "rage",
+  "bugs",
+  "html",
+  "json",
+  "music",
+  "ideas",
+  "faith",
+  "atoms",
+  "magic",
+  "humor",
+  "colors",
+  "regret",
+  "coffee",
+  "dreams",
+  "hatred",
+  "vscode",
+  "node.js",
+  "science",
+  "buttons",
+  "respect",
+  "madness",
+  "quality",
+  "daisyUI",
+  "morality",
+  "devotion",
+  "tailwind",
+  "stupidity",
+  "sacrilege",
+  "dark mode",
+  "intensity",
+  "community",
+  "simplicity",
+  "dedication",
+  "randomness",
+  "javascript",
+  "efficiency",
+  "melancholy",
+  "[REDACTED]",
+  "intelligence",
+  "carelessness",
+];
+
+function randomizeWord() {
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  document.getElementById("the-word")?.setAttribute("innerHTML", randomWord);
+}
+
+randomizeWord();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const themeButtons = document.querySelectorAll(".theme");
+
+  function applyTheme(theme: string) {
+    if (theme === "light") document.documentElement.setAttribute("data-theme", "light");
+    else if (theme === "dark") document.documentElement.setAttribute("data-theme", "dark");
+    else document.documentElement.removeAttribute("data-theme");
+  }
+
+  function saveTheme(theme: string) {
+    localStorage.setItem("theme", theme);
+  }
+
+  function loadIconColor() {
+    const favicon = document.querySelector("link[rel='icon']");
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+      favicon?.setAttribute("href", "./assets/resources/icon-dark.svg");
+    else favicon?.setAttribute("href", "./assets/resources/icon.svg");
+  }
+
+  function handleSystemThemeChange() {
+    const currentTheme = localStorage.getItem("theme") || "default";
+    if (currentTheme === "default") {
+      loadIconColor();
+      applyTheme("default");
+    }
+  }
+
+  const savedTheme = localStorage.getItem("theme") || "default";
+  applyTheme(savedTheme);
+
+  themeButtons.forEach((button) => {
+    if ((button as HTMLInputElement).value === savedTheme) (button as HTMLInputElement).checked = true;
+
+    button.addEventListener("change", () => {
+      const selectedTheme = (button as HTMLInputElement).value;
+      applyTheme(selectedTheme);
+      saveTheme(selectedTheme);
+    });
+  });
+
+  loadIconColor();
+
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", handleSystemThemeChange);
+  window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", handleSystemThemeChange);
+});
